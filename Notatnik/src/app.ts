39115756e8e0db50942a7bb8d92626c553cdaa5f
bartlomiejk97 <kocikbartlomiej@gmail.com>
2021-05-17 19:23:55 +0200

    date i obiekt

--- Notatnik/src/app.ts
+++ Notatnik/src/app.ts
@@ -1,104 +1,46 @@
-
 export class App {
-    
-    containerBox: HTMLElement;
+    noteArray : object[] = [];
+    noteContainer: HTMLElement;
+
     constructor() {
-         /*this.arrayWeather = this.getData();
-        this.inputsEvents();
-        this.containerBox = document.getElementById("weatherBox");
-        this.show();
-        localStorage.clear();*/
-        this.addNewNote();
+        this.bindEventAddNewNote();
     }
-    addNewNote(){
-        const AddNoteButton = document.getElementById('AddNote') as HTMLButtonElement;
-        AddNoteButton.addEventListener('submit',(e) => this.onSubmit(e));
-    }
-    onSubmit(e: any){
-        e = 0;
-        e ++;
-        console.log(e);
-    }
-
-    /*inputsEvents(): void {
-        const cityForm = document.getElementById('cityForm');
-        const cityButton = document.getElementById('cityButton') as HTMLButtonElement;
-        cityForm.addEventListener('submit', (e) => this.onSubmit(e));
-    }
-    onSubmit(e: any) {
-        e.preventDefault();
-        //console.log(this.getInputValue()); // tu wyświelta nazwę z inputa
-        const cityName = this.getInputValue().trim();
-        if(cityName !=="")
-        {
-            this.getCityInfo(cityName);
-        }
-    }
-    getInputValue(): string {
-        const cityInput = document.getElementById('cityName') as HTMLInputElement;
-        const cityName = cityInput.value;
-        return cityName || "";
-    }
-    async getCityInfo(cityName: string): Promise<void> {
-        const weather = await this.getWeather(cityName);
-        this.arrayWeather.push(weather);
-        this.saveData(this.arrayWeather);
-        //console.log(this.arrayWeather); // wyświetla tablice
-
-    }
-
-    async getWeather(cityName: string): Promise<any> {
-        const apiUrl: string = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${this.opwApiKey}&&units=metric`;
-        const weatherResponse = await fetch(apiUrl);
-        const weatherData = await weatherResponse.json();
-        // console.log(weatherData); 
-        return weatherData;
-    }
-
-    saveData(data: any): void {
-        localStorage.setItem('weatherData', JSON.stringify(data));
-        this.show();
-    }
-    getData(): any {
-        const data = localStorage.getItem('weatherData');
-        if (data) {
-            return JSON.parse(data);
-        } else {
-            return [];
-        }
-
-    }
-    show() {
-        this.printWeatherBox(this.arrayWeather);
-    }
-
-    printWeatherBox(weatherData: any) {
-        console.log(weatherData);
-        let weatherItems = "";
-        weatherData.forEach((element: any) => {
-            //console.log(element.main);
-            weatherItems += `
-                <div class="create">
-                    <p> Nazwa : ${element.name}</p>
-                    <p> Temperatura : ${element.main.temp}°C</p>
-                    <p> Wilgotność : ${element.main.humidity}%</p>
-                    <button id=${element.id} class="remove"> Usuń </button>
-                </div>
-            `;
+    bindEventAddNewNote() : void {
+        const addNoteButton = document.getElementById('AddNote') as HTMLButtonElement;
+        addNoteButton.addEventListener('click',(event:MouseEvent) => {
+            event.preventDefault();
+            this.getNoteValue();
         });
-        this.containerBox.innerHTML = weatherItems;
-        document.querySelectorAll(`.remove`)?.forEach((element: any) => element.addEventListener(`click`, (e: any) => this.dropCity(e)));
     }
-    dropCity(e: any) {
-        //console.log("before",this.arrayWeather);
-        this.arrayWeather = this.arrayWeather.filter((item) => {
-            console.log(item.name, item.id.toString() !== e.target.id);
-            return item.id.toString() !== e.target.id;
-        })
-        // console.log("after",this.arrayWeather);
+    getNoteValue() : void {
+        
+        const titleNoteValue : HTMLInputElement= document.querySelector('.title');
+        const textAreaValue : HTMLTextAreaElement = document.querySelector('.textarea');
+        const textArea = textAreaValue.value;
+        const titleNote = titleNoteValue.value;
+        const newNote : object = {
+            title: titleNote,
+            text: textArea,
+            noteDate:this.getDayNotes()
+        }
+        this.noteArray.push(newNote);
+        console.log(this.noteArray);
+    }
+    getDayNotes() : string {
+        const now : Date = new Date();
+        const noteDate : string = `${now.getDate()}.${now.getMonth()+1}.${now.getFullYear()}`;
+        return noteDate;
+    }
+    
+    createNewNote(){
+        tu stowrze nową notatkę 
+        append child i creacte new element
+    }
+       
+      
+  
 
-        this.saveData(this.arrayWeather);
-        this.show();
-        //console.log(this.arrayWeather);
-    }*/
+
 }
+
+
