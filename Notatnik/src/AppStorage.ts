6d853857cea053a9dd239057cea7f018cbdeeaf5
bartlomiejk97 <kocikbartlomiej@gmail.com>
2021-05-19 21:07:45 +0200

    change color + basic css, remove, pinned button but I dont know how yet

---
+++ Notatnik/src/AppStorage.ts
@@ -0,0 +1,35 @@
+import { App } from "./app";
+export class AppStorage{
+
+
+    constructor(){
+    }
+    // savaData(noteArray:any){
+    //     noteArray.forEach((element:object) => {
+    //         localStorage.setItem('notePinned', JSON.stringify(element));
+    //     });
+    // }
+    // removeDate(notePinned: any){
+
+    // }
+    // getData(): any {
+    //     const notePinned = localStorage.getItem('notePinned');
+    //     if (notePinned) return JSON.parse(notePinned);
+    // }
+    // save data 
+    // zapisywac
+    // usuwać z local i html
+}
+
+
+// dropCity(e: any) {
+//     //console.log("before",this.arrayWeather);
+//     this.arrayWeather = this.arrayWeather.filter((item) => {
+//         console.log(item.name, item.id.toString() !== e.target.id);
+//         return item.id.toString() !== e.target.id;
+//     })
+//     // console.log("after",this.arrayWeather);
+
+//     this.saveData(this.arrayWeather);
+//     this.show();
+//     //console.log(this.arrayWeather);
